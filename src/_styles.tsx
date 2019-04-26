a47fea8996db748380e75f5fe06cd34a2143dadd
Karlos Santos <lgvichy@gmail.com>
2019-04-26 02:22:43 -0700

    Basic setup

---
+++ src/_styles.tsx
@@ -0,0 +1,17 @@
+import { createGlobalStyle } from 'styled-components'
+
+export const theme = {
+  primaryColor: '#bc7eb6',
+  primaryVariantColor: '#481346',
+  secondaryColor: '#90298d',
+  secondaryVariantColor: '#231f20',
+  background: '#fff',
+  hoverBackground: 'rgba(1,1,1,0.2)',
+  disabledText: '#aaa',
+  error: '#b00020',
+  backgroundOpacity: 0.33,
+  backgroundZIndex: 1000,
+  titleFont: 'Righteous',
+  subtitleFont: 'Roboto',
+  titleHeight: window.innerWidth > 700 ? 80 : 57
+}
