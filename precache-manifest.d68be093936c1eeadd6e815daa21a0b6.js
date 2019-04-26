055eaf8cf320492d3673e190361817f4e57aa580
Karlos Santos <lgvichy@gmail.com>
2019-04-26 02:50:25 -0700

    Updates

---
+++ precache-manifest.d68be093936c1eeadd6e815daa21a0b6.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "59d5cc766ff8e2d84d5085d132ee4bb7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ade965b9c8284aa824cb",
+    "url": "/static/js/2.614d0c9e.chunk.js"
+  },
+  {
+    "revision": "c0f0cca999fef687758b",
+    "url": "/static/js/main.7dae49bf.chunk.js"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.c5541365.js"
+  }
+]);
